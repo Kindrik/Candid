e2864569dafe938469af675f600497ad584e1041
Josh <joshua.winters@tsymmetry.com>
2023-09-06 11:48:06 -0400

    initial test on pulling the top 100 stars into a console.log works, i have a returned array of 100 objects. Now I need to loop that array into a MUI card for display.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
 const App = () => {
+
+  const [stars, setStars] = useState([]);
+
+  const getRepos = async () => {
+    try {
+      const res = await fetch('https://api.github.com/search/repositories?q=stars:%3E30000&per_page=100')
+      const data = await res.json()
+      return data
+    } catch (e) {
+      return false
+    }
+  }
+
+  useEffect(() => {
+		async function getStars() {
+			const res = await getRepos()
+			if (res === false) {
+				setStars(false)
+			}
+			setStars(res.items);
+		}
+		getStars();
+	}, []);
+  
   return (
     <main>
-
+      {console.log('test response:', stars)}
     </main>
   );
 }
