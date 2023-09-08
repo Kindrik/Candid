872b3e68b1597b56d3e0143e7338304b04ecad14
Josh <joshua.winters@tsymmetry.com>
2023-09-07 22:24:40 -0400

    added the cards, and mapped through the array. Also added a grid with set viewport sizes, desktop 3 across, tablet 2, phone 1. Added some simple padding. Next iteration i will add the header toolbar, and work with the second half, the commits. Then clean up, splitting components etc.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 
 const App = () => {
 
@@ -7,12 +12,12 @@ const App = () => {
   const getRepos = async () => {
     try {
       const res = await fetch('https://api.github.com/search/repositories?q=stars:%3E30000&per_page=100')
-      const data = await res.json()
+      const data = await res.json();
       return data
     } catch (e) {
       return false
     }
-  }
+  };
 
   useEffect(() => {
 		async function getStars() {
@@ -28,6 +33,25 @@ const App = () => {
   return (
     <main>
       {console.log('test response:', stars)}
+      <Grid container spacing={2}>
+      {stars.map((stars) => {
+
+        return (
+          <Grid xs={12} sm={12} md={6} lg={4} sx={{padding:'20px'}}>
+          <Card sx={{ display: 'flex' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <CardContent sx={{ flex: '1 0 auto' }}>
+                <Typography component="div" variant="h5">{stars.name}</Typography>
+                <Typography variant="subtitle1" color="text.secondary" component="div">{stars.url}</Typography>
+                <Typography variant="subtitle2" color="text.secondary" component="div">{stars.stargazers_count}</Typography>
+              </CardContent>
+            </Box>
+          </Card>
+          </Grid>
+        )
+
+      })}
+      </Grid>
     </main>
   );
 }
