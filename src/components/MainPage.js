3e4e2aac198d51d55d44304543db12140346d430
Josh <joshua.winters@tsymmetry.com>
2023-09-11 19:50:00 -0400

    added react router, and set up a navbar that is responsive, and 2 dummy pages, plus the main page. A few bugs to fix here, and eslint semantics, but otherwise, moving along well

---
+++ src/components/MainPage.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import GitHubCard from './GithubCard';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Typography from '@mui/material/Typography';
+import Header from './header';
+
+
+
+const getRepos = async () => {
+	try {
+		const res = await fetch('https://api.github.com/search/repositories?q=stars:%3E30000&per_page=100')
+		const data = await res.json()
+		return data
+	} catch (e) {
+		return false
+	}
+}
+
+const GitHubStars = () => {
+	const [ repos, setRepos ] = useState([])
+
+	const yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString()
+	useEffect(() => {
+		async function get() {
+			const res = await getRepos()
+			if (res === false) {
+				setRepos(false)
+			}
+			setRepos(res.items)
+		}
+		get()
+	}, [])
+	if (repos === false || repos.message) {
+		return <Typography>Uh-Oh, GitHub API seems to be down...</Typography>
+	} 
+	if (repos && repos.length === 0) {
+		return <Typography sx={{textAlign:'center',marginTop:'20px'}}>Getting Repos...</Typography>
+	}
+	return (		
+    <>
+        <Header />
+        <Grid container spacing={2} sx={{margin:'20px'}}>
+            { repos.map(repo => (
+                    <Grid xs={12} md={6} lg={4}>
+                    <GitHubCard key={repo.name} repo={repo} yesterday={yesterday} />
+                    </Grid>
+            ))}
+        </Grid>
+    </>
+)
+}
+
+export default GitHubStars
---
+++ src/components/header.js
@@ -0,0 +1,114 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function Header() {
+
+    const [anchorElNav, setAnchorElNav] = useState(null);
+	const [anchorElUser, setAnchorElUser] = useState(null);
+  
+	const handleOpenNavMenu = (event) => {
+	  setAnchorElNav(event.currentTarget);
+	};
+
+	const handleCloseNavMenu = () => {
+	  setAnchorElNav(null);
+	};
+  
+	const handleCloseUserMenu = () => {
+	  setAnchorElUser(null);
+	};
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+        <Container maxWidth="xl">
+            <Toolbar disableGutters>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{display: { xs: 'block', md: 'none' },}}
+                    >
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                        <Link to="profile">Profile</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                            <Link to="about">About</Link>
+                        </Typography>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Typography textAlign="center">
+                        <Link to="/">Homepage</Link>
+                        </Typography>
+                    </MenuItem>
+                    </Menu>
+                </Box>
+<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+  <Button onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>Homepage</Button>
+  <Button onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>Profile</Button>
+  <Button onClick={handleCloseNavMenu} sx={{ my: 2, color: 'white', display: 'block' }}>About</Button>
+</Box>
+
+<Box sx={{ flexGrow: 0 }}>
+<Menu
+  sx={{ mt: '45px' }}
+  id="menu-appbar"
+  anchorEl={anchorElUser}
+  anchorOrigin={{
+    vertical: 'top',
+    horizontal: 'right',
+  }}
+  keepMounted
+  transformOrigin={{
+    vertical: 'top',
+    horizontal: 'right',
+  }}
+  open={Boolean(anchorElUser)}
+  onClose={handleCloseUserMenu}
+>
+    <MenuItem onClick={handleCloseUserMenu}>
+      <Typography textAlign="center">Homepage</Typography>
+    </MenuItem>
+
+</Menu>
+</Box>
+</Toolbar>
+</Container>
+</AppBar>
+</Box>
+  )
+}
