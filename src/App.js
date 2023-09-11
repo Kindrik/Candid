d9bd15175cb7a0a60d4cd77f7153a5aa4d5656c3
Josh <joshua.winters@tsymmetry.com>
2023-09-11 15:31:53 -0400

    added a newer appbar, one that actually works on mobile. Links dont work yet, implementing the SPA router later tonight.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import Container from '@mui/material/Container';
+import MenuItem from '@mui/material/MenuItem';
+
 
 const getRepos = async () => {
 	try {
@@ -19,8 +23,28 @@ const getRepos = async () => {
 	}
 }
 
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
 const GitHubStars = () => {
 	const [ repos, setRepos ] = useState([])
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
+  
+	const handleOpenNavMenu = (event) => {
+	  setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+	  setAnchorElUser(event.currentTarget);
+	};
+  
+	const handleCloseNavMenu = () => {
+	  setAnchorElNav(null);
+	};
+  
+	const handleCloseUserMenu = () => {
+	  setAnchorElUser(null);
+	};
 
 	const yesterday = new Date(new Date().getTime() - (24 * 60 * 60 * 1000)).toISOString()
 	useEffect(() => {
@@ -42,20 +66,85 @@ const GitHubStars = () => {
 	return (
 		<>
 			<Box sx={{ flexGrow: 1 }}>
-      			<AppBar position="static">
-        			<Toolbar>
-          				<IconButton
-            			size="large"
-            			edge="end"
-            			color="inherit"
-            			aria-label="menu"
-            			sx={{ mr: 2 }}
-          			>
-            			<MenuIcon />
-          				</IconButton>
-          				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>Top 100 Starred Repos On Github</Typography>
-        			</Toolbar>
-      			</AppBar>
+			<AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
     		</Box>
 			<Grid container spacing={2} sx={{margin:'20px'}}>
 				{ repos.map(repo => (
